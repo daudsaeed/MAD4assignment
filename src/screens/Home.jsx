e208064daf0989767e9dc740e78ab6ce61515d4e
daudsaeed <dawoodsaeed.studies@gmail.com>
2023-01-04 00:42:26 +0500

    fetching the comments

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,22 +1,57 @@
 import { useState, useEffect } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 import colors from "../constants/colors";
 
 const Home = ({ navigation }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [comments, setComments] = useState();
   useEffect(() => {
+    setIsLoading(true);
+
+    // fetching the data
     fetch("http://jsonplaceholder.typicode.com/comments")
       .then((response) => response.json())
-      .then((comments) => console.log(comments))
-      .catch((error) => console.log(error));
-  });
-  const [isLoading, setIsLoading] = useState();
+      .then((comments) => {
+        console.log(comments);
+        setComments(comments);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }, []);
   return (
     <View style={styles.container}>
-      <View style={styles.commentsHeader}>
-        <Text>All Comments</Text>
-      </View>
+      {isLoading ? (
+        <ActivityIndicator size={"large"} color={colors.primary} />
+      ) : (
+        <View style={styles.commnetsContainer}>
+          <View style={styles.commentsHeader}>
+            <Text>All Comments</Text>
+          </View>
 
-      <View style={styles.comments}></View>
+          <View style={styles.comments}>
+            <FlatList
+              keyExtractor={(item) => item.id}
+              data={comments}
+              renderItem={({ item }) => {
+                return (
+                  <View style={styles.comment}>
+                    <Text>{item.name}</Text>
+                  </View>
+                );
+              }}
+            />
+          </View>
+        </View>
+      )}
     </View>
   );
 };
@@ -27,6 +62,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: colors.white,
+  },
+
+  commnetsContainer: {
+    margin: 20,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  comment: {
+    backgroundColor: "#eee",
+    padding: 10,
+    borderRadius: 4,
+    marginBottom: 20,
+    shadowColor: "#000",
   },
 });
 export default Home;
